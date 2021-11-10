afd0e28c19347ebddbc8a3770c143792c1d03da6
MariiaDenysova <maria.denadel@gmail.com>
2021-11-10 15:02:33 +0200

    add total sum for expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,6 +4,7 @@ class ExpensesController < ApplicationController
   # GET /expenses or /expenses.json
   def index
     @expenses = Expense.all
+    @calculated_expense_sum = Expense.sum(:amount)
   end
 
   # GET /expenses/1 or /expenses/1.json
